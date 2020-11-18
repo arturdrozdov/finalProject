b5260addfe7e3fc8f0bfc0495bcf08aae05dde69
Drozdov Artur <artur.drozdov11@gmail.com>
2020-11-18 16:10:48 +0200

    +Connect

--- src/main/java/com/drozdov/ChatBot.java
+++ src/main/java/com/drozdov/ChatBot.java
@@ -47,21 +47,6 @@ public class ChatBot {
 
 
         }
-
-        public static int calculateMonthCount(int dreamCost, int professionSalary) {
-            int monthCount = dreamCost / professionSalary;
-            monthCount = validateMonthCount(monthCount);
-            return monthCount;
-        }
-
-        public static int validateMonthCount(int monthCount) {
-            if (monthCount == 0) {
-                return 1;
-            }
-
-            return monthCount;
-        }
-
         private static boolean isHelloMessage(String message) {
             message = message.toLowerCase();
 
